cb365d128f7459a70ed11be34bfe00c752b4243a
mnoqc <nmomnr@gmail.com>
2024-05-16 20:49:31 +0900

    회원가입 완료하기

--- src/main/java/com/mnoqc/member/entity/MemberEntity.java
+++ src/main/java/com/mnoqc/member/entity/MemberEntity.java
@@ -1,6 +1,7 @@
 package com.mnoqc.member.entity;
 
 
+import com.mnoqc.member.dto.MemberDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,4 +24,11 @@ public class MemberEntity {
     @Column
     private String memberName;
 
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
 }
--- src/main/java/com/mnoqc/member/service/MemberService.java
+++ src/main/java/com/mnoqc/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.mnoqc.member.service;
 
 import com.mnoqc.member.dto.MemberDTO;
+import com.mnoqc.member.entity.MemberEntity;
 import com.mnoqc.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,10 @@ public class MemberService {
 
     // 회원 정보 저장 메서드
     public void save(MemberDTO memberDTO) {
-
-
+        // 1. dto -> entity 변환
+        // 2. repository의 save 메서드 호출
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+        // repository의 save 메서드 호출 (조건: Entity 객체를 넘겨 줘야 함)
     }
 }
