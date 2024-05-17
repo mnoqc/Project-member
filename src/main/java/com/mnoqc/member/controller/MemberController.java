00340cb842ea9aa7416a0114ecca8a3f6ba48270
mnoqc <nmomnr@gmail.com>
2024-05-17 20:58:24 +0900

    ajax 이메일 입력 정보 서버 전달 #17

--- src/main/java/com/mnoqc/member/controller/MemberController.java
+++ src/main/java/com/mnoqc/member/controller/MemberController.java
@@ -94,4 +94,10 @@ import java.util.List;
         return "index";
     }
 
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+        System.out.println("memberEmail = " + memberEmail);
+        return "체크 완료";
+    }
+
 }
