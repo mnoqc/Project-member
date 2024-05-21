33af2491a54b84799562e0475bc11e1604b6dfb0
mnoqc <nmomnr@gmail.com>
2024-05-21 17:54:07 +0900

    게시판 단일파일 첨부 구현

--- src/main/java/com/mnoqc/member/controller/BoardController.java
+++ src/main/java/com/mnoqc/member/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class BoardController {
         return "boardSave";
     }
     @PostMapping("/save")
-    public String save(@ModelAttribute BoardDTO boardDTO) {
+    public String save(@ModelAttribute BoardDTO boardDTO) throws IOException {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
