f8e470b1cc94c1beb67899ff080dc8a398ce02ed
mnoqc <nmomnr@gmail.com>
2024-05-21 18:41:34 +0900

    게시판 단일파일 첨부 구현_마무리

--- src/main/java/com/mnoqc/member/dto/BoardDTO.java
+++ src/main/java/com/mnoqc/member/dto/BoardDTO.java
@@ -2,17 +2,21 @@ package com.mnoqc.member.dto;
 
 import com.mnoqc.member.entity.BaseEntity;
 import com.mnoqc.member.entity.BoardEntity;
+import com.mnoqc.member.entity.BoardFileEntity;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+
+// DTO(Data Transfer Object), VO, Bean,         Entity
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor // 기본 생성자
+@NoArgsConstructor // 기본생성자
 @AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
-// DTO(Data Transfer Object), VO, Bean
 public class BoardDTO {
     private Long id;
     private String boardWriter;
@@ -25,7 +29,7 @@ public class BoardDTO {
 
     private MultipartFile boardFile; // save.html -> Controller 파일 담는 용도
     private String originalFileName; // 원본 파일 이름
-    private String storedFileName; //  서버 저장용 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름
     private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
@@ -51,7 +55,7 @@ public class BoardDTO {
         } else {
             boardDTO.setFileAttached(boardEntity.getFileAttached()); // 1
             // 파일 이름을 가져가야 함.
-            // originalFileName, storedFileName : board_file_table(BoardFileEntity)
+            // orginalFileName, storedFileName : board_file_table(BoardFileEntity)
             // join
             // select * from board_table b, board_file_table bf where b.id=bf.board_id
             // and where b.id=?
